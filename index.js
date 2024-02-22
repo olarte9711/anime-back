fc4f9d1570fa0af505f057eb1d7259db1c15c30b
Óscar Olarte <oscar.ovargas@mercadolibre.com.co>
2024-02-22 05:23:52 -0500

    Add cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors'
 import { getAnimeRecommendation } from './services/animeService.js';
 
 const app = express();
+
+app.use(cors())
 
 const port = process.env.PORT || 3000;
 
